d521ec50c3274fe016fc5a40d8a62cc25eff1b90
Avi Sananes <avi.sananes@aidoc.com>
2022-04-25 18:30:26 +0300

    spearated data completness parts

--- data_completeness.py
+++ data_completeness.py
@@ -3,18 +3,17 @@ from sklearn import impute
 
 """
 What I would add:
-1. Documentation
+1. Documentation - done
 2. Maybe seperate into smaller functions or just add comments on what happens
-    in each part.
+    in each part. - done 
 3. After all of this - we still have a lot of rows with nans, 
     can we complete it somehow? (I saw it happend to me but not to you in the 
-    notebook so maybe something is missing in this code)
+    notebook so maybe something is missing in this code) - partially done
 4. C & D consts - I'm not sure how we got the values and if they are correct,
     maybe add documentation? I remember we didn't agree on the D value
 """
 
 D_FILLNA_VAL = -999
-C_FILLNA_VAL = 200
 KNN_IMPUTE_NEIGHBORS = 5
 INT_COLS = ['Month', 'device', 'num_of_admin_pages', 'num_of_info_pages',
             'num_of_product_pages', 'Region']
@@ -25,42 +24,77 @@ DUR_COL_DICT = {
 }
 
 
-def fill_missing_data(df: pd.DataFrame, int_cols: list, dur_col_dict: dict):
-
+def fill_duration_zeros(df: pd.DataFrame):
+    """
+     Fills duration columns with zeros where:
+     1. The total duration equals zero.
+     2. The relevant page visit number equal zero.
+    """
     for key in DUR_COL_DICT:
         df[key] = df.apply(
-            lambda row: row[key] == 0 if row["total_duration"] == 0 else row[key],
-            axis=1
-            )
+        lambda row: row[key] == 0 if row['total_duration'] == 0 else row[key],
+        axis=1
+        )
         df[key] = df.apply(
-            lambda row: row[key] == 0 if row[dur_col_dict[key]] == 0 else row[key],
-            axis=1
-            )
+        lambda row: row[key] == 0 if row[DUR_COL_DICT[key]] == 0 else row[key],
+        axis=1
+        )
+    
+    df['info_page_duration'] = df.apply(
+        lambda row: row['info_page_duration'] == 0 if row['total_duration'] == row['admin_page_duration'] or row['total_duration'] == row['product_page_duration'] else row['info_page_duration'],
+        axis=1
+        )
+    df['admin_page_duration'] = df.apply(
+        lambda row: row['admin_page_duration'] == 0 if row['total_duration'] == row['info_page_duration'] or row['total_duration'] == row['product_page_duration'] else row['admin_page_duration'],
+        axis=1
+        )
+    df['product_page_duration'] = df.apply(
+        lambda row: row['product_page_duration'] == 0 if row['total_duration'] == row['admin_page_duration'] or row['total_duration'] == row['info_page_duration'] else row['product_page_duration'],
+        axis=1
+        )
+    # TODO: Better way for this?
+    return df
 
-    df["D"] = df["D"].fillna(D_FILLNA_VAL)
 
-    df['C'] = df['C'].fillna(C_FILLNA_VAL)
+def fill_special_cols(df: pd.DataFrame):
+    """
+     Fills the colums "A", "C" and "D" with values that won't interrupt with the analysis.
+    """
     df['A'] = df['A'].fillna('c_0')
-    df['C'] = df['C'].fillna(df['C'].mode())
-
-    # TODO - maybe for columns with "logX"
-    #  it's better to have the expression result instead of X itself?
-
     df['A'] = df['A'].str.extract('(\d+)', expand=False)
     df['A'] = df['A'].astype(float)
 
     df['C'] = df['C'].str.extract('(\d+)', expand=False)
+    df['C'] = df['C'].fillna(df['C'].mode())
+    df['C'] = df['C'].fillna(200) #TODO: figure out why it isnt filled without the actual number.
     df['C'] = df['C'].astype(float)
 
+    df["D"] = df["D"].fillna(D_FILLNA_VAL)
+
+    return df
+
+
+def impute_knn_missing_data(df: pd.DataFrame):
+    """
+     Fills all missing values with KNN imputer.
+    """
     imputer = impute.KNNImputer(n_neighbors=KNN_IMPUTE_NEIGHBORS)
-    filled_train_data = pd.DataFrame(imputer.fit_transform(df), columns=df.columns)
+    filled_df = pd.DataFrame(imputer.fit_transform(df), columns=df.columns)
 
-    for col in int_cols:
-        filled_train_data[col] = filled_train_data[col].apply(lambda x: round(x))
+    for col in INT_COLS:
+        filled_df[col] = filled_df[col].apply(lambda x: round(x))
 
-    filled_train_data['total_duration'] = filled_train_data.apply(
+    return filled_df
+
+
+def fill_total_duration(df: pd.DataFrame):
+    """
+     Fills the total duration column with the sum of other duration columns.
+    """
+    df['total_duration'] = df.apply(
         lambda row: row['info_page_duration'] + row['product_page_duration'] + row[
             'admin_page_duration'], axis=1
         )
-
+    
     return df
+
